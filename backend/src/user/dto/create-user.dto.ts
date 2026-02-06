05e25f744cbadf742883ce10e0057754254b451c
Luiz Loch <118476368+Luiz-Loch@users.noreply.github.com>
2026-02-06 16:34:06 -0300

    feat: initialize user module structure and dtos

--- backend/src/user/dto/create-user.dto.ts
+++ backend/src/user/dto/create-user.dto.ts
@@ -1,1 +1,15 @@
-export class CreateUserDto {}
+import { IsEmail, IsString, IsStrongPassword, Length } from "class-validator";
+
+export class UserCreateDto {
+
+    @IsString()
+    @Length(2, 100)
+    public readonly name: string;
+    
+    @IsEmail()
+    @Length(2, 100)
+    public readonly email: string;
+
+    @IsStrongPassword()
+    public readonly password: string;
+}
--- backend/src/user/dto/update-user.dto.ts
+++ backend/src/user/dto/update-user.dto.ts
@@ -1,4 +1,14 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
+import { IsEmail, IsOptional, IsString, Length } from "class-validator";
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UserUpdateDto {
+
+    @IsString()
+    @Length(2, 100)
+    @IsOptional()
+    public readonly name?: string;
+
+    @IsEmail()
+    @Length(2, 100)
+    @IsOptional()
+    public readonly email?: string;
+}

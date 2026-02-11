34ca8353058886e4daabb4da4ac7ef30ee271ff9
Luiz Loch <118476368+Luiz-Loch@users.noreply.github.com>
2026-02-11 10:39:02 -0300

    feat: auth controller and dtos documentation

--- backend/src/auth/dto/auth-token-response.dto.ts
+++ backend/src/auth/dto/auth-token-response.dto.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class AuthTokenResponseDto {
+  @ApiProperty({ description: 'The access token for authentication' })
   public readonly accessToken: string;
 
   public constructor(accessToken: string) {
     this.accessToken = accessToken;
   }
-  
+
 }
 
--- backend/src/auth/dto/login.dto.ts
+++ backend/src/auth/dto/login.dto.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsStrongPassword } from 'class-validator';
 
 export class LoginDto {
   @IsEmail()
+  @ApiProperty({ description: 'The email address of the user' })
   email: string;
 
   @IsStrongPassword()
+  @ApiProperty({ description: 'The password of the user' })
   password: string;
 }

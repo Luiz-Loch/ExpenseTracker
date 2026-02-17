1686fa30974af9fc08fb342a80ab2ca909a14422
Luiz Loch <118476368+Luiz-Loch@users.noreply.github.com>
2026-02-17 12:02:11 -0300

    feature: common compoents

---
+++ frontend/app/components/common/AppLoading.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="d-flex justify-center align-center" :style="{ minHeight: `${minHeight}px` }">
+    <v-progress-circular indeterminate color="primary" :size="size" />
+  </div>
+</template>
+
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    minHeight?: number
+    size?: number
+  }>(),
+  {
+    minHeight: 400,
+    size: 48,
+  },
+)
+</script>
